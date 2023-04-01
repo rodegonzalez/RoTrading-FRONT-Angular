240004bc59ff2b66ce072cf58ae984da387bf77a
Rode Gonzalez <rode.gonzalez@gmail.com>
2023-04-01 23:12:25 +0100

    bg-light

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <app-nav-bar></app-nav-bar>
-    <div class="container" style="margin-top: 120px;">
-      <router-outlet></router-outlet>
-    </div>
-    <app-footer></app-footer>
-  `,
+  templateUrl: './app.component.html',
   styles: []
 })
 export class AppComponent {}
