22de9f9bc1891a3b0613bfeaab87c110cea3ec56
Rode Gonzalez <rode.gonzalez@gmail.com>
2024-08-16 14:51:27 +0100

    positions-list-one row:hover

--- src/app/components/op-components/positions/position-list-one/position-list-one.component.ts
+++ src/app/components/op-components/positions/position-list-one/position-list-one.component.ts
@@ -3,10 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-position-list-one',
   templateUrl: './position-list-one.component.html',
-  styles: [
+  styleUrls: ['./position-list-one.component.css'
   ]
 })
 export class PositionListOneComponent {
   @Input() position: any;
-
 }
